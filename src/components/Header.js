f004e2724833efdb80ba0685e5d2e16cc75acff7
Sari Stissi <sestissi16@earlham.edu>
2020-06-01 16:48:56 -0700

    styled and made the header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Nav, Navbar } from "react-bootstrap";
+import './Header.css'
 
 const Header = () => {
   return (
     <div id="headerContainer">
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Chocolatine</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="#about" className="nav-link">About Us</Nav.Link>
-                <Nav.Link href="#menu" className="nav-link">Menu</Nav.Link>
-                <Nav.Link href="#contact" className="nav-link">Contact Us</Nav.Link>
-                <Nav.Link href="#order" className="nav-link">Order</Nav.Link>
-            </Nav>
+        <Navbar variant="dark" expand="lg" sticky="top" className="custom-navbar-bg">
+            <Navbar.Brand href="#home" className="navbar-title">Chocolatine - Artisan Bakery</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse  id="basic-navbar-nav">
+                <Nav className="navLinksContainer">
+                    <Nav.Link href="#about" className="navbar-link">About Us</Nav.Link>
+                    <Nav.Link href="#menu" className="navbar-link">Menu</Nav.Link>
+                    <Nav.Link href="#contact" className="navbar-link">Contact Us</Nav.Link>
+                    <Nav.Link href="#order" className="navbar-link">Order</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     </div>
   );

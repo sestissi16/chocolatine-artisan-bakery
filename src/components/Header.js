cbe0599f72db6d11fd1006ace2363c7c36aced22
Sari Stissi <sestissi16@earlham.edu>
2020-06-01 18:29:43 -0700

    added a home intro section to header and styled it

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,29 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div id="headerContainer">
-        <Navbar variant="dark" expand="lg" sticky="top" className="custom-navbar-bg">
-            <Navbar.Brand href="#home" className="navbar-title">Chocolatine - Artisan Bakery</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse  id="basic-navbar-nav">
-                <Nav className="navLinksContainer">
-                    <Nav.Link href="#about" className="navbar-link">About Us</Nav.Link>
-                    <Nav.Link href="#menu" className="navbar-link">Menu</Nav.Link>
-                    <Nav.Link href="#contact" className="navbar-link">Contact Us</Nav.Link>
-                    <Nav.Link href="#order" className="navbar-link">Order</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </div>
+    <header id="home">
+        <div id="headerContainer">
+            <Navbar variant="dark" expand="lg" sticky="top" className="custom-navbar-bg">
+                <Navbar.Brand href="#home" className="navbar-title">Chocolatine - Artisan Bakery</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse  id="basic-navbar-nav">
+                    <Nav className="navLinksContainer">
+                        <Nav.Link href="#about" className="navbar-link">About Us</Nav.Link>
+                        <Nav.Link href="#menu" className="navbar-link">Menu</Nav.Link>
+                        <Nav.Link href="#contact" className="navbar-link">Contact Us</Nav.Link>
+                        <Nav.Link href="#order" className="navbar-link">Order</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <div id="homeSection">
+                <div id="homeInfo">
+                    <h1>Welcome to <span className="spanTitle">Chocolatine</span></h1>
+                    <br/>
+                    <h2 id="homeTagline">We make French baked goods with love, so kickstart your day with us!</h2>
+                </div>
+            </div>
+        </div>
+    </header>
   );
 };
 

d4ea07b4c7b8f9a5690aadda432e849fb0784d08
Sari Stissi <sestissi16@earlham.edu>
2020-06-01 19:10:23 -0700

    adding scripts to create smooth scrolling

---
+++ public/js/init.js
@@ -0,0 +1,14 @@
+// Smooth Scrolling for Navigation Bar
+
+$('.smoothscroll').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+    $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 800, 'swing', function () {
+        window.location.hash = target;
+    });
+});

4d236a73bddc0b46b73bbc155f05c3feed038d5b
Sari Stissi <sestissi16@earlham.edu>
2020-06-03 12:14:01 -0700

    fixed header title so it wouldn't look wonky when resized

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header id="home">
         <div id="headerContainer">
             <div id="homeInfo">
-                <h1>Welcome to <span className="spanTitle">Chocolatine</span></h1>
+                <h1 id="homeTitle">Welcome to <span className="spanTitle">Chocolatine</span></h1>
                 <br/>
                 <h2 id="homeTagline">We make French baked goods with love, so kickstart your day with us!</h2>
                 <br/>

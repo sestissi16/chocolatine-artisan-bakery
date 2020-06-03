3f2d5d48dcf76e3dfce567f853cf9f7157039d4e
Sari Stissi <sestissi16@earlham.edu>
2020-06-02 20:02:47 -0700

    added a list of food possibilities and styled it

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,8 @@ const Header = () => {
                 <h1>Welcome to <span className="spanTitle">Chocolatine</span></h1>
                 <br/>
                 <h2 id="homeTagline">We make French baked goods with love, so kickstart your day with us!</h2>
+                <br/>
+                <h3 id="homeList"> We have warm muffins, buttery biscuits, tasty breads and delicious viennoiseries, and many other delicacies!</h3>
             </div>
         </div>
     </header>

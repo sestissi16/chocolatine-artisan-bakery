212f74b7c5224eafb37640209c6af21c1a5c248d
Sari Stissi <sestissi16@earlham.edu>
2020-06-05 20:29:45 -0700

    added decorative border to header and made it responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,15 @@ const Header = () => {
     <header id="home">
         <div id="headerContainer">
             <div id="homeInfo">
-                <h1 id="homeTitle">Welcome to <span className="spanTitle">Chocolatine</span></h1>
-                <br/>
-                <h2 id="homeTagline">We make French baked goods with love, so kickstart your day with us!</h2>
-                <br/>
-                <h3 id="homeList"> We have warm muffins, buttery biscuits, tasty breads and delicious viennoiseries, and many other delicacies!</h3>
+              <div id="homeInfoBorderImage">
+                <div id="homeInfoText">
+                  <h1 id="homeTitle">Welcome to <span className="spanTitle">Chocolatine</span></h1>
+                  <br/>
+                  <h2 id="homeTagline">We make French baked goods with love, so kickstart your day with us!</h2>
+                  <br/>
+                  <h3 id="homeList"> We have warm muffins, buttery biscuits, tasty breads and delicious viennoiseries, and many other delicacies!</h3>
+                </div>
+              </div>
             </div>
         </div>
     </header>

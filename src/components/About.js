9b268c93ce68447e60b180caeb093dd3eac68966
Sari Stissi <sestissi16@earlham.edu>
2020-06-08 15:41:48 -0700

    added border images and styled about section with ornamental borders

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import BlackLineDivider from '../assets/blackFlowerDivider-Transparent-Cropped.png'
+import TitleBorderTop from '../assets/goldOrnateLines-Top4-Transparent.png'
+import TitleBorderBottom from '../assets/goldOrnateLines-Bottom4-Transparent.png'
 import './About.css'
 
 const About = () => {
   return (
     <section id="about">
         <div id="aboutContainer">
-          <h1 id="aboutSectionTitle">About Us:</h1>
+          <div id="aboutSectionTitle">
+            <img src={TitleBorderTop} alt="gold ornamental border" id="aboutTitleTopBorder"/>
+            <h1 id="aboutTitleText">About Us:</h1>
+            <img src={TitleBorderBottom} alt="gold ornamental border" id="aboutTitleBottomBorder"/>
+          </div>
           <div id="aboutRow1">
             <div id="aboutMission">
               <h2 className="aboutTitles">Our Mission:</h2>
+                <img src={BlackLineDivider} alt="ornamental divider" className="aboutLineDivider"/>
                 <h4>We've grown up eating <span className="aboutEmphasis">incredible French baked goods</span> and we wanted to share that with all of you.</h4><br/>
                 <h4>
                   That's why our mission is to bring you incredible baked goods that are as <span className="aboutEmphasis">fresh</span> as possible and <span className="aboutEmphasis">made with love</span>, 
@@ -17,6 +25,7 @@ const About = () => {
             </div>
             <div id="aboutServices">
               <h2 className="aboutTitles">Our Services:</h2>
+              <img src={BlackLineDivider} alt="ornamental divider" className="aboutLineDivider"/>
               <h3 id="aboutServicesList">Catering, Wholesale, Events, Small Batch Orders</h3>
               <h4>
                 Do you want a couple goodies for a quick breakfast? 
@@ -30,6 +39,7 @@ const About = () => {
           <div id="aboutRow2">
             <div id="aboutHowWeMakeIt">
               <h2 className="aboutTitles">How We Make Our Goods:</h2>
+              <img src={BlackLineDivider} alt="ornamental divider" className="aboutLineDivider"/>
               <h4>
                 We work with <span className="aboutEmphasis">local</span> suppliers to source the <span className="aboutEmphasis">freshest and most authentic ingredients</span> to reduce the preservatives and flavorless, overly processed products in your food.
               </h4>
@@ -40,6 +50,7 @@ const About = () => {
             </div>
             <div id="aboutHowToOrder">
               <h2 className="aboutTitles">How To Order Our Delicious Goods:</h2>
+              <img src={BlackLineDivider} alt="ornamental divider" className="aboutLineDivider"/>
               <h3>You can:</h3>
               <ul className="aboutHowToList">
                 <h4><li className="aboutHowToListItems">Message us on <a href="https://www.facebook.com/ArtisanBakeries/" className="aboutLink">Facebook</a></li></h4>

e0566c5eccd2a3af92ef89458f3c9c261ada87f2
Sari Stissi <sestissi16@earlham.edu>
2020-11-18 22:56:32 -0800

    added icons, moved info to bakeryData, finished styling

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,8 @@ import bakeryInfo from '../bakeryData'
 import TitleFrame from '../assets/backgrounds/aboutUsDarkBrown.png'
 import TitleFrameSm from '../assets/backgrounds/aboutUsDarkBrownSm.png'
 import servicesIcon from '../assets/Images/varietyMixOurServices.png'
+import madeIcon from '../assets/Images/varietyMixHowMade.png'
+import orderIcon from '../assets/Images/cashRegister.png'
 
 class About extends Component {
     render(){
@@ -29,11 +31,36 @@ class About extends Component {
         if(bakeryInfo.aboutUs.serviceSum) {
             var serviceSum = bakeryInfo.aboutUs.serviceSum;
         }
-        // var process1 = bakeryInfo.aboutUs.process1;
-        // var process2 = bakeryInfo.aboutUs.process2;
-        // var order1 = bakeryInfo.aboutUs.order1;
-        // var order2 = bakeryInfo.aboutUs.order2;
-        // var order3= bakeryInfo.aboutUs.order3;
+        if(bakeryInfo.aboutUs.process1) {
+            var process1 = bakeryInfo.aboutUs.process1;
+        }
+        if(bakeryInfo.aboutUs.process2) {
+            var process2 = bakeryInfo.aboutUs.process2;
+        }
+        if(bakeryInfo.aboutUs.process3) {
+            var process3 = bakeryInfo.aboutUs.process3;
+        }
+        if(bakeryInfo.aboutUs.processSum) {
+            var processSum = bakeryInfo.aboutUs.processSum;
+        }
+        if(bakeryInfo.aboutUs.order1) {
+            var order1 = bakeryInfo.aboutUs.order1;
+            var order1Link = bakeryInfo.aboutUs.order1Link;
+            var order1LinkName = bakeryInfo.aboutUs.order1LinkName;
+        }
+        if(bakeryInfo.aboutUs.order2) {
+            var order2 = bakeryInfo.aboutUs.order2;
+            var order2Link = bakeryInfo.aboutUs.order2Link;
+            var order2LinkName = bakeryInfo.aboutUs.order2LinkName;
+        }
+        if(bakeryInfo.aboutUs.order3) {
+            var order3 = bakeryInfo.aboutUs.order3;
+            var order3Link = bakeryInfo.aboutUs.order3Link;
+            var order3LinkName = bakeryInfo.aboutUs.order3LinkName;
+        }
+        if(bakeryInfo.aboutUs.orderExtraInfo) {
+            var orderExtraInfo = bakeryInfo.aboutUs.orderExtraInfo;
+        }
   return (
     <section id="about">
         <div id="aboutContainer">
@@ -56,41 +83,70 @@ class About extends Component {
                     <img src={servicesIcon} alt="Images of a bag of flour a croissant and a roll" id="aboutServicesIcon" />
                     <div id="aboutServices">
                         <h1 className="aboutTitles">Our Services:</h1>
-                        <h3 id="aboutServicesList">{serviceList}</h3>
-                        <h4>
-                            {serviceEx1} <br/>
-                            {serviceEx2} <br/>
-                            {serviceEx3}
-                        </h4><br/>
-                        <h4>{serviceSum}</h4><br/>
-                        <h5 id="aboutServicesContact">Contact us on <a href="https://www.facebook.com/ArtisanBakeries/" className="aboutLink">Facebook</a> or by <a href="/Contact" className="aboutLink">Email</a> for more details about Catering, Wholesale, and Events</h5>
+                        <h2 id="aboutServicesList">{serviceList}</h2>
+                        <h3 id="aboutServicesEx">
+                            <ul>
+                                <li>
+                                    <h3>{serviceEx1}</h3>
+                                </li>
+                                <li>
+                                    <h3>{serviceEx2}</h3>
+                                </li>
+                                <li>
+                                    <h3>{serviceEx3}</h3>
+                                </li>
+                            </ul>
+                        </h3>
+                        <h3>{serviceSum}</h3><br/>
+                        <h4 id="aboutServicesContact">
+                            Contact us on 
+                            <a href="https://www.facebook.com/ArtisanBakeries/" className="aboutLink"> Facebook </a> 
+                            or by 
+                            <a href="/Contact" className="aboutLink"> Email </a> 
+                            for more details about Catering, Wholesale, and Events
+                        </h4>
                     </div>
                 </div>
                 <img src={BlackLineDivider} alt="ornamental divider" className="aboutLineDivider"/>
                 <div id="aboutHowRow">
                     <div id="aboutHowWeMakeIt">
-                        <h2 className="aboutTitles">How We Make Our Goods:</h2>
-                        <h4>
+                        <h1 className="aboutTitles">How We Make Our Goods:</h1>
+                        <h3>
+                            <ul>
+                                <li>
+                                    <h3>{process1}</h3>
+                                </li>
+                                <li>
+                                    <h3>{process2}</h3>
+                                </li>
+                                <li>
+                                    <h3>{process3}</h3>
+                                </li>
+                            </ul>
+                        </h3>
+                        <h3>{processSum}</h3>
+                        {/* <h4>
                             We work with <span className="aboutEmphasis">local</span> suppliers to source the <span className="aboutEmphasis">freshest and most authentic ingredients</span> to reduce the preservatives and flavorless, overly processed products in your food.
-                        </h4>
-                        <br/>
-                        <h4>
+                        </h4> */}
+                        {/* <h4>
                             We also use <span className="aboutEmphasis">old-fashioned techniques</span> to ensure the <span className="aboutEmphasis">highest quality</span>.
-                        </h4>
+                        </h4> */}
                     </div>
+                    <img src={madeIcon} alt="Images of grains of wheat, rolling pin, and whisk" id="aboutHowIcon" />
                 </div>
                 <img src={BlackLineDivider} alt="ornamental divider" className="aboutLineDivider"/>
                 <div id="aboutOrderRow">
+                    <img src={orderIcon} alt="Drawing of a cash register" id="aboutOrderIcon" />
                     <div id="aboutHowToOrder">
-                        <h2 className="aboutTitles">How To Order Our Delicious Goods:</h2>
-                        <h3>You can:</h3>
+                        <h1 className="aboutTitles">How To Order Our Delicious Goods:</h1>
+                        <h2>You can:</h2>
                         <ul className="aboutHowToList">
-                            <h4><li className="aboutHowToListItems">Message us on <a href="https://www.facebook.com/ArtisanBakeries/" className="aboutLink">Facebook</a></li></h4>
-                            <h4><li className="aboutHowToListItems">Use the <a href="#order" className="aboutLink smoothscroll">Order Form</a> on this webiste</li></h4>
-                            <h4><li className="aboutHowToListItems"><a href="#contact" className="aboutLink smoothscroll">Email us</a> (artisanbakeriesusa@gmail.com)</li></h4>
+                            <h3><li className="aboutHowToListItems">{order1}<a href={order1Link} className="aboutLink">{order1LinkName}</a></li></h3>
+                            <h3><li className="aboutHowToListItems">{order2}<a href={order2Link} className="aboutLink smoothscroll">{order2LinkName}</a></li></h3>
+                            <h3><li className="aboutHowToListItems"><a href={order3Link} className="aboutLink smoothscroll">{order3LinkName}</a>{order3}</li></h3>
                         </ul>
                         <br/>
-                        <h4>When you order, please let us know the date and time you'd like your order by and how many of which items you'd like.</h4>
+                        <h3>{orderExtraInfo}</h3>
                     </div>
                 </div>
             </div>

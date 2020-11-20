f268ddf0d32f86e6a4ad00b8a5da7578b7500c3b
Sari Stissi <sestissi16@earlham.edu>
2020-11-19 21:13:01 -0800

    added new logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Header.css'
-
+import RectangleLogo from '../assets/Logos/chocolatineTransparentLogo.png'
 class Header extends Component {
 
     render () {
@@ -9,7 +9,11 @@ class Header extends Component {
                 <div id="headerContainer">
                     <div id="homeInfo">
                         <div id="homeInfoBorderImage">
+                            
                             <div id="homeInfoText">
+                            <div id="homeLogo">
+                                <img src={RectangleLogo} alt="Chocolatine logo with a croissant icon" id="homeLogoImage"/>
+                            </div>
                             <h1 id="homeTitle">Welcome to <span className="spanTitle">Chocolatine</span></h1>
                             <br/>
                             <h2 id="homeTagline">We make European baked goods with love, so kickstart your day with us!</h2>

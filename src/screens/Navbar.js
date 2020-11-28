1d6c4b4b76982bbb0539414ee605ce4d2b3d4c22
Sari Stissi <sestissi16@earlham.edu>
2020-11-28 17:43:50 -0600

    aded react-icons, moved order to end, changed order to cart

--- src/screens/Navbar.js
+++ src/screens/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { FiShoppingCart } from "react-icons/fi"
 import './Navbar.css'
 
 const Navigation = () => {
@@ -11,8 +12,18 @@ const Navigation = () => {
                 <Nav className="navLinksContainer">
                     <Nav.Link href="/About" className="navbar-link">About Us</Nav.Link>
                     <Nav.Link href="/Menu" className="navbar-link">Menu</Nav.Link>
-                    <Nav.Link href="/Order" className="navbar-link">Order</Nav.Link>
                     <Nav.Link href="/Contact" className="navbar-link">Contact Us</Nav.Link>
+                    <OverlayTrigger
+                        placement="bottom"
+                        overlay={
+                            <Tooltip id={`tooltip-cart`}>
+                                Your Cart.
+                            </Tooltip>
+                        }
+                    >
+                        <Nav.Link href="/Cart" className="navbar-link"> <FiShoppingCart/> </Nav.Link>
+                    </OverlayTrigger>
+                    
                 </Nav>
             </Navbar.Collapse>
         </Navbar>

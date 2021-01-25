9eb6bec95bdfcb01d426569ff1ed12d4fd0e2e01
Sari Stissi <sestissi16@earlham.edu>
2021-01-24 20:18:42 -0800

    added about data, changed background images, changed styling for about

--- src/screens/About.js
+++ src/screens/About.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Navigation from './Navbar'
 import BlackLineDivider from '../assets/borders/black/blackFlowerDivider-Transparent-Cropped.png'
 import './About.css'
+import { FaCarSide, FaPeopleCarry } from 'react-icons/fa'
+import { MdRestaurantMenu } from 'react-icons/md'
 import bakeryInfo from '../bakeryData'
 import yourOrder from '../yourOrder'
 import CircleLogo from '../assets/Logos/chocolatineTransparentCircleLogo.png'
@@ -14,78 +16,76 @@ import orderIcon from '../assets/Images/cashRegister.png'
 class About extends Component {
     render(){
         var orderCount = yourOrder.orderList.count
-        var missionTagline = bakeryInfo.aboutUs.missionTagline
-        var missionTitle = bakeryInfo.aboutUs.missionTitle
-        var missionDescript = bakeryInfo.aboutUs.missionDescript
-        var missionOrderDescript = bakeryInfo.aboutUs.missionOrderDescript
-        if(bakeryInfo.aboutUs.service1) {
-            var service1 = bakeryInfo.aboutUs.service1;
-            var service1Ex = bakeryInfo.aboutUs.service1Ex;
-        }
-        if(bakeryInfo.aboutUs.service2) {
-            var service2 = bakeryInfo.aboutUs.service2;
-            var service2Ex = bakeryInfo.aboutUs.service2Ex;
-        }
-        if(bakeryInfo.aboutUs.service3) {
-            var service3 = bakeryInfo.aboutUs.service3;
-            var service3Ex = bakeryInfo.aboutUs.service3Ex;
-        }
-        if(bakeryInfo.aboutUs.order1) {
-            var order1 = bakeryInfo.aboutUs.order1;
-            var order1Link = bakeryInfo.aboutUs.order1Link;
-            var order1LinkName = bakeryInfo.aboutUs.order1LinkName;
-        }
-        if(bakeryInfo.aboutUs.order2) {
-            var order2 = bakeryInfo.aboutUs.order2;
-            var order2Link = bakeryInfo.aboutUs.order2Link;
-            var order2LinkName = bakeryInfo.aboutUs.order2LinkName;
-        }
-        if(bakeryInfo.aboutUs.order3) {
-            var order3 = bakeryInfo.aboutUs.order3;
-            var order3Link = bakeryInfo.aboutUs.order3Link;
-            var order3LinkName = bakeryInfo.aboutUs.order3LinkName;
-        }
-        if(bakeryInfo.aboutUs.orderExtraInfo) {
-            var orderExtraInfo = bakeryInfo.aboutUs.orderExtraInfo;
-        }
+        const {
+            missionTagline,
+            missionDescript,
+            missionDescript2,
+            service1,
+            service1Ex,
+            service2,
+            service2Ex,
+            service3,
+            service3Ex,
+            service4,
+            service5,
+            service6,
+            giveBack1,
+            giveBack2,
+            giveBack3,
+            giveBack4,
+            actionTitle,
+            actionDescript
+        } = bakeryInfo.aboutUs
   return (
     <section id="about">
         <Navigation orderCount={orderCount}/>
         <div id="aboutContainer">
             <div id="aboutSectionHeader">
-                {/* <img src={TitleFrame} alt="About Us Title with Vintage Frame" id="aboutTitleImage" class="tabletDesktopSize"/>
-                <img src={TitleFrameSm} alt="About Us Title with Vintage Frame" id="aboutTitleImage" class="mobileSize"/> */}
                 <div id="aboutSectionTitle">
                     <div id="aboutTitleDiv">
-                        <img src={TitleFrame} alt="Fancy frame with About written in it" id="aboutTitleImage"/>
+                        <h1 id="aboutTitleText">About Us</h1>
+                        {/* <img src={TitleFrame} alt="Fancy frame with About written in it" id="aboutTitleImage"/> */}
                     </div>
+                    <img src={CircleLogo} alt="chocolatine circle logo" id="aboutLogo" />
                     <h1 id="aboutTagline">{missionTagline}</h1>
+                    <div id="aboutMissionText">
+                        <h2 id="aboutMissionDescrip">{missionDescript}</h2>
+                        <h3 id="aboutMissionOrder">{missionDescript2}</h3>
+                    </div>
                 </div>
-            </div>
-            <div id="aboutMission">
-                <div id="aboutMissionText">
-                    <h2 id="aboutMissionTitle">{missionTitle}</h2>
-                    <h3 id="aboutMissionDescrip">{missionDescript}</h3>
-                    <h3 id="aboutMissionOrder">{missionOrderDescript}</h3>
-                </div>
-                <img src={CircleLogo} alt="chocolatine circle logo" id="aboutLogo" />
             </div>
             <div id="aboutServiceRow">
                 <div id="aboutServiceBg">
                     <div id="aboutServices">
                         <h1 className="aboutTitles">What We Do:</h1>
                         <div id="aboutServicesEx">
-                            <div className="aboutServiceItem">
-                                <h2 className="serviceTitle">{service1}</h2>
-                                <h3 className="serviceEx">{service1Ex}</h3>
+                            <div className="aboutServicesColumn">
+                                <div className="aboutServiceItem">
+                                    <h2 className="serviceTitle">{service1}</h2>
+                                    <h3 className="serviceEx">{service1Ex}</h3>
+                                </div>
+                                <div className="aboutServiceItem">
+                                    <h2 className="serviceTitle">{service2}</h2>
+                                    <h3 className="serviceEx">{service2Ex}</h3>
+                                </div>
+                                <div className="aboutServiceItem">
+                                    <h2 className="serviceTitle">{service3}</h2>
+                                    <h3 className="serviceEx">{service3Ex}</h3>
+                                </div>
                             </div>
-                            <div className="aboutServiceItem">
-                                <h2 className="serviceTitle">{service2}</h2>
-                                <h3 className="serviceEx">{service2Ex}</h3>
-                            </div>
-                            <div className="aboutServiceItem">
-                                <h2 className="serviceTitle">{service3}</h2>
-                                <h3 className="serviceEx">{service3Ex}</h3>
+                            <div className="aboutServicesColumn">
+                                <div className="aboutServiceItem">
+                                    <FaPeopleCarry className="serviceIcon" />
+                                    <h2 className="serviceTitle">{service4}</h2>
+                                </div>
+                                <div className="aboutServiceItem">
+                                    <FaCarSide className="serviceIcon" />
+                                    <h2 className="serviceTitle">{service5}</h2>
+                                </div>
+                                <div className="aboutServiceItem">
+                                    <MdRestaurantMenu className="serviceIcon" />
+                                    <h2 className="serviceTitle">{service6}</h2>
+                                </div>
                             </div>
                         </div>
                         <h4 id="aboutServicesContact">
@@ -96,6 +96,26 @@ class About extends Component {
                             for more details about Catering, and Wholesale
                         </h4>
                     </div>
+                </div>
+            </div>
+            <div id="aboutGiveBack">
+                <div id="aboutGiveBackBg">
+                    <h1 className="aboutTitles">How we want to give back:</h1>
+                    <div className="aboutGiveRows">
+                        <div className="aboutGiveItem"><h2>{giveBack1}</h2></div>
+                        <div className="aboutGiveItem"><h2>{giveBack2}</h2></div>
+                    </div>
+                    <div className="aboutGiveRows">
+                        <div className="aboutGiveItem"><h2>{giveBack3}</h2></div>
+                        <div className="aboutGiveItem"><h2>{giveBack4}</h2></div>
+                    </div>
+                </div>
+            </div>
+            <div id="aboutAction">
+                <div id="aboutActionBg">
+                    <h2 id="aboutActionTitle" className="aboutTitles">{actionTitle}</h2>
+                    <h3 id="aboutActionDescript">{actionDescript}</h3>
+                    <a href="/Contact" id="aboutContactBtn">Contact Us</a>
                 </div>
             </div>
             {/* <div id="aboutPhotoMosaic">
